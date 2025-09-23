f7ebde2df06318d463eb9896790ec9086944f7f0
Caio Samuel <samuel451200@gmail.com>
2025-09-23 14:29:02 -0300

    Correção

--- Mainv3.java
+++ Mainv3.java
@@ -1,18 +1,18 @@
-public class Mainv3.4 {
+public class Mainv3 {
     public static void main(String[] args) {
         int dia = 1;
-        int mes = 1
-        int ano = 2017
+        int mes = 1;
+        int ano = 2017;
 
         while (ano <= 2020) {
             mes = 1;
             while (mes <= 12) {
                 dia = 1;
                 while (dia <= 30) {
-                    System.out.prinln(dia + '/' + mes + '/' + ano);
+                    System.out.println(dia + '/' + mes + '/' + ano);
                     dia += 1;
                 mes += 1;
-            ano + = 1;
+            ano += 1;
                 }
             }
         }
