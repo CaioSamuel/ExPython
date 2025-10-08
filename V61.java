2c1aabb9508b1eaec653248d1c24fd3752634089
Caio Samuel <samuel451200@gmail.com>
2025-10-08 17:42:54 -0300

    Add Java and Python examples for random, loop, and factorial
    Added V61.java to generate 6 random numbers, V62.java to print a number triangle based on user input, and teste.py to calculate the factorial of a number. These files demonstrate basic Java and Python programming concepts.

---
+++ V61.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class V61 {
+    public static void main(String[] args) {
+        Random gerador = new Random();
+
+        int qtdNums = 0;
+        while (qtdNums < 6) {
+            System.out.println(gerador.nextInt(50));
+            qtdNums++;
+        }
+        }
+    }
