7307b9c5ae88d373729118852e5076da716d6c20
Samuelmumu <samuel451200@gmail.com>
2025-11-26 07:24:00 -0300

    Add Flask app for user management and reorganize project
    Introduced a new Flask application with a main route and a template for user management. Deleted Equipe.py, and moved project files from ExercíciosCodeInsights to the Proj directory for better organization.

---
+++ Flask/Main.py
@@ -0,0 +1,22 @@
+from flask import Flask, url_for, render_template
+
+#Inicialização
+app = Flask(__name__)
+
+#Rotas
+@app.route('/')
+def ola_mundo():
+    titulo = "Gestão de Usuários"
+    usuarios = [
+        {"nome": "Guilherme", "membro_ativo": True},
+        {"nome": "João", "membro_ativo": False},
+        {"nome": "Maria", "membro_ativo": False},
+    ]
+    return render_template('index.html', titulo=titulo, usuarios=usuarios)
+
+@app.route('/sobre')
+def pagina_sobre():
+    return 'Programador'
+
+#Execução
+app.run(debug = True)
