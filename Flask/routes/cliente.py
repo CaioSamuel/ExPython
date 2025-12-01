af0d173f28427877d578400abedf69f5cf792fcd
Samuelmumu <samuel451200@gmail.com>
2025-11-30 23:17:36 -0300

    Refactor Flask app structure and clean up routes
    Refactored the Flask application to use Blueprints, moving home and client routes to separate modules. Removed unused form and data handling logic, deleted the 'formulario.html' template and 'dados.txt' data file, and simplified 'index.html' to focus on client management. Also resolved merge conflict markers in Proj/Main.py.

--- Flask/routes/cliente.py
+++ Flask/routes/cliente.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 cliente_route = Blueprint('cliente', __name__)
 
@@ -16,9 +16,41 @@ Rota de Clientes
 
 @cliente_route.route('/')
 def lista_clientes():
+    ''' listar os clientes'''
+    return render_template('lista_clientes.html')
+
+
+@cliente_route.route('/', methods=['POST'])
+def inserir_clientes():
+    ''' Inserir os dados do cliente '''
     pass
 
 
+@cliente_route.route('/new', methods=['GET'])
+def form_cliente():
+    ''' Formulário para cadastrar um cliente'''
+    return render_template('form_cliente.html')
+
+
 @cliente_route.route('/<int:cliente_id>')
-def obter_clientes(cliente_id):
+def detalhe_cliente(cliente_id):
+    ''' exibir detalhes de um cliente  '''
+    return render_template('detalhe_cliente.html')
+
+
+@cliente_route.route('/<int:cliente_id>/edit')
+def form_edit_cliente(cliente_id):
+    ''' formulario para editar um cliente  '''
+    return render_template('form_edit_cliente.html')
+
+
+@cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
+def atualizar_cliente(cliente_id):
+    ''' atualizar informações do cliente '''
+    pass
+
+
+@cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
+def deletar_cliente(cliente_id):
+    ''' deletar informações do cliente '''
     pass
